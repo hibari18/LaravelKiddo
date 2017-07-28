4547bcd649bcfa43719055dbfb42714de106288a
amiel <amielgolosinda@ymail.com>
2017-07-28 22:53:32 +0800

    Update on changeDiv()

--- resources/views/section/section.blade.php
+++ resources/views/section/section.blade.php
@@ -42,7 +42,14 @@
             
         </div>
         
-        @include('section.select.level')
+        <div class="form-group" style="margin-top: 10%">
+          <label class="col-sm-4" style="text-align: right">Level Name</label>
+          <div class="col-sm-7 selectContainer">
+            <select class="form-control" name="addLvlSelect" id="addLvlSelect" disabled style="width: 100%;">
+              <option selected value = '0'>--Select Level--</option>
+            </select>
+          </div>
+        </div> 
 
         <div class="form-group" style="margin-top: 20%">
                 <label class="col-sm-4" style="text-align: right">Section</label>
--- resources/views/section/select/level.blade.php
+++ resources/views/section/select/level.blade.php
@@ -1,11 +1,6 @@
-        <div class="form-group" style="margin-top: 10%">
-                <label class="col-sm-4" style="text-align: right">Level Name</label>
-                <div class="col-sm-7 selectContainer">
-                <select class="form-control" name="addLvlSelect" id="addLvlSelect" disabled style="width: 100%;">
+
                 <option selected value = '0'>--Select Level--</option>
                         @foreach($levels as $level)
                         <option value="{{ $level->tblLevelId}}">{{ $level->tblLevelName }}</option>
                         @endforeach
-                </select>
-                </div>
-        </div> 
+                
