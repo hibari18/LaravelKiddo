c47f43d75548cea0ef57c9362d9c91863eedd7dc
Gwyneth <gwenalbania11@gmail.com>
2017-08-14 11:40:40 -0700

    Health Information Form fix (Admission)

--- resources/views/admission/stepfour.blade.php
+++ resources/views/admission/stepfour.blade.php
@@ -1,5 +1,5 @@
 
-<div class="col-md-offset">
+<div class="col-md-12 col-sm-12 col-xs-12">
     <h3>Health History</h3>
      
         <div class="form-group">
@@ -25,7 +25,7 @@
 
         <div class="form-group">
           <label class="col-lg-4 control-label"  style="text-align: right">Blood Type:</label>
-            <div class="col-lg-8"  style="margin-bottom: 2%">
+            <div class="col-lg-8"  style="margin-bottom: 2%; width: 35%">
               <select class="form-control" name="selHealthBtype" id="selHealthBtype">
                 <option value="A">A</option>
                 <option value="B">B</option>
@@ -34,23 +34,38 @@
               </select>
             </div>
         </div>
-
+    
+      <hr>
           <div class="form-group">
-            <label class="col-lg-4 control-label" style="text-align: right">Hospitalized?
-              <label style="margin-left: 3%">Yes
-                <input type="radio" name="h2" class="minimal" checked>
+            <label class="control-label" style="margin-left: 10%">Hospitalized?
+            
+              <label style="margin-left: 5%">
+                <input type="radio" name="h2" class="minimal" checked>Yes
+                <input type="radio" name="h2" class="minimal" onchange="disabledReason()">No
               </label>
-              <label>No
-                <input type="radio" name="h2" class="minimal" onchange="disabledReason()">
-              </label>
+              
+           
             </label>
           </div>
         
+
             <div class="form-group">
               <label class="col-lg-4 control-label"  style="text-align: right">Reason:</label>
               <div class="col-lg-8"  style="margin-bottom: 2%">
                 <input class="form-control" type="text" name="txtHealthReason" id="txtHealthReason">
               </div>
+            </div>
+
+            <hr style="margin-top: 10%">
+
+            <div class="form-group" style="margin-left: 20%">
+              <label class="control-label">In case of emergency, can we call your family doctor/pediatrician?
+                <label style="margin-left: 3%">
+                  <input type="radio" name="r1" class="minimal" checked>Yes
+                  <input type="radio" name="r1" class="minimal" onchange="disabledEmergency()">No
+                </label>
+               
+              </label>
             </div>
 
             <div class="form-group">
@@ -69,7 +84,7 @@
 
             <div class="form-group">
               <label class="col-lg-4 control-label" style="text-align: right">Tel/Mobile #:</label>
-              <div class="col-lg-8" style="margin-bottom: 2%">
+              <div class="col-lg-8" style="margin-bottom: 2%; width: 35%">
                 <input class="form-control" type="text" name="txtHealthHosNum" id="txtHealthHosNum">
               </div>
             </div>
